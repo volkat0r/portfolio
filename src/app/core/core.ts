89ba9424400eb62daaa98c7968802011db40a617
volkat0r <volkerderksen@gmail.com>
2026-01-22 13:17:29 +0100

    folder structure

---
+++ src/app/core/core.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core',
+  imports: [],
+  templateUrl: './core.html',
+  styles: ``,
+})
+export class Core {
+
+}
---
+++ src/app/core/features/skills/skills.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  imports: [],
+  templateUrl: './skills.html',
+  styles: ``,
+})
+export class Skills {
+
+}
